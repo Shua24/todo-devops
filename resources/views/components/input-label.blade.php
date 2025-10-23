0ae3e6c4ae1189cf08efc075e47d1564d8da1103
shua24 <jo.daniel.simanjuntak@gmail.com>
2025-10-23 10:44:37 +0700

    views: Ubah elemen button dan landing page

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,6 @@
 @props(['value'])
 
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-300']) }}>
+<label
+    {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700 dark:text-gray-300 focus:ring-green-500']) }}>
     {{ $value ?? $slot }}
 </label>
