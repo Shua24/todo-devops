2f35357e6a4aecc974dabeb8f6a3cf2d9ac70b9f
shua24 <jo.daniel.simanjuntak@gmail.com>
2025-12-31 02:42:57 +0700

    Tambah script untuk helm chart

---
+++ helm-charts/helm.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update
+
+helm install ingress-nginx ingress-nginx/ingress-nginx \
+    --namespace ingress-nginx --create-namespace
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+helm install prometheus prometheus-community/kube-prometheus-stack \
+    --namespace monitoring --create-namespace
